06343378a76c4f7d124d9ff736e172787b5695fc
YaelCohen23 <71295974+YaelCohen23@users.noreply.github.com>
2020-10-20 21:26:26 -0400

    Update avengersMarkov.js

--- avengersMarkov.js
+++ avengersMarkov.js
@@ -295,3 +295,131 @@ notes: [
 totalTime: 20
 };
 
+function genNGram(text, n) {
+  var ngrams = {};
+  // Look at all characters of the String
+  for (var i = 0; i < text.length - n; i++) {
+    // Look at an ngram
+    var gram = text.substring(i, i + n);
+    // Look at the next character
+    var next = text.charAt(i + n);
+    // If this is a new one, make an empty array
+    if (!ngrams.hasOwnProperty(gram)) {
+      ngrams[gram] = [];
+    }
+    // Add the next character as a possible outcome
+    ngrams[gram].push(next);
+  }
+  console.log(ngrams);
+  return ngrams;
+}
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+function choice(somelist) {
+    var i = getRandomInt(0,somelist.length);
+    return somelist[i];
+  }
+
+function midiToFreq(m) {
+    return Math.pow(2, (m - 69) / 12) * 440;
+}
+
+function midiToChar(m) {
+  return String.fromCharCode(m);
+}
+
+function playNotes(noteList) {
+    noteList = mm.sequences.unquantizeSequence(noteList)
+    console.log(noteList.notes)
+    noteList.notes.forEach(note => {
+        playNote(note);
+    });
+}
+
+function playNote(note) {
+    offset = 1 
+    gainNode.gain.setTargetAtTime(0.8, note.startTime+offset, 0.01)
+    osc.frequency.setTargetAtTime(midiToFreq(note.pitch), note.startTime+offset, 0.001)
+    gainNode.gain.setTargetAtTime(0, note.endTime+offset-0.05, 0.01)
+
+}
+
+function charToMidi(c) {
+  return c.charCodeAt();
+}
+
+function genMarkNotes(n) {
+  var noteList = HIGH_STRINGS.notes;
+  var outNoteList = [];
+  var text = "";
+  for (var i = 0; i < noteList.length; i++) {
+    text += midiToChar(noteList[i].pitch);
+  }
+
+  console.log(text)
+
+  ngrams = genNGram(text, n);
+
+  var current = "";
+  var offset = 0;
+
+  // Start with an arbitrary ngram
+  for (var i = 0; i < n; i++) {
+    current += String.fromCharCode(noteList[i].pitch);
+    outNoteList.push({pitch: noteList[i].pitch, startTime: offset, endTime: offset +1});
+    offset++;
+  }
+  // The final text
+  var output = current;
+  // Generate a new character some number of times
+  for (var i = 0; i < 20; i++) {
+    // If this is a valid ngram
+    console.log("current: "  + current);
+    if (ngrams.hasOwnProperty(current)) {
+      // What are all the possible next tokens
+      var possible = ngrams[current];
+      // Pick one randomly that take into account the probabilities of the markov chains generated
+      var next = choice(possible);
+      // Add to the output
+      output += next;
+      console.log("output " + output);
+      outNoteList.push({pitch: charToMidi(next),startTime: offset, endTime: offset + 1});
+      offset ++;
+      // Get the last N entries of the output; we'll use this to look up
+      // an ngram in the next iteration of the loop
+      current = output.substring(output.length-n, output.length);
+    } 
+    else {
+      break;
+    }
+  }
+  return outNoteList;
+
+}
+
+
+const playButton = document.querySelector('button');
+playButton.addEventListener('click', function() {
+    audioCtx = new (window.AudioContext || window.webkitAudioContext)
+    osc = audioCtx.createOscillator();
+    gainNode = audioCtx.createGain();
+    osc.connect(gainNode).connect(audioCtx.destination);
+    osc.start()
+    gainNode.gain.value = 0;
+
+    var n = 0;
+
+    n = document.getElementById("nValue").value;
+
+    console.log("n: " + n);
+
+    noteList = genMarkNotes(Number(n));
+    noteList.forEach(note => {
+        playNote(note);
+    });
+}, false);
